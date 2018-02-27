fec14390616d4c47b61bbc2d2e1fdd0a7c8619c3
Jiri Masek <jirka.mk@gmail.com>
2018-02-27 03:56:17 +0100

    doplneni komentare

--- moje-csas-aplikace/src/main/java/com/csas/mojeCsasAplikace/controller/PageController.java
+++ moje-csas-aplikace/src/main/java/com/csas/mojeCsasAplikace/controller/PageController.java
@@ -10,7 +10,7 @@ public class PageController {
 	@RequestMapping(value = { "", "/home", "/index" }) //vsechno s timhle URL pujde pres tuhle metodu
 	public ModelAndView index() {
 
-		ModelAndView mv = new ModelAndView("page");
+		ModelAndView mv = new ModelAndView("page"); // dispatcher-servlet preklada na page.jsp
         mv.addObject("greeting", "Welcome to Spring Web MVC");
 		return mv;
 	}
